67997cd2f6102cab05d2968480c51e7f381c5042
Chang <cnxiong03@gmail.com>
2020-11-20 22:17:43 -0500

    print game board

---
+++ src/com/codewithchang/Main.java
@@ -0,0 +1,22 @@
+package com.codewithchang;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}};
+
+	            printGameBoard(gameBoard);
+            }
+    public static void printGameBoard(char[][] gameBoard) {
+        for(char[] row : gameBoard) {
+            for(char c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+}
